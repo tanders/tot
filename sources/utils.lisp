c640d70efebb8d948d890477e18680b06ba2da27
Torsten Anders <torsten.anders@beds.ac.uk>
2017-11-09 23:16:32 +0000

    Moved two util funs into ta-utilities system/library

--- sources/utils.lisp
+++ sources/utils.lisp
@@ -17,34 +17,6 @@
   (intern (symbol-name key) :om))
 
 
-;;; TODO: turn into project independent function with doc etc. It is already general enough.
-;; Based on https://groups.google.com/forum/#!topic/comp.lang.lisp/xDaUVFDnp5w
-(defun map-neighbours (func list &optional n)
-  "Applying `func' to consecutive sublists of `list'. The number of arguments expected by func implicitly specifies the number of consecutive elements to which the function is applied. This can be overwritten by the optional `n'."
-  (let ((n2 (if n n (length (ccl:arglist func)))))
-    (loop for l on list 
-      when (>= (length l) n2)
-      collect (apply func (subseq l 0 n2)))))
-
-#|
-(map-neighbours #'(lambda (x y) (list x y)) 
-                '(1 2 3 4 5 6))
-=> ((1 2) (2 3) (3 4) (4 5) (5 6))
-
-(map-neighbours #'+ '(1 2 3 4 5 6)
-                3)
-=> (6 9 12 15)
-
-Note: doing the same with loop
-
-(loop
-  for (key value) on '(1 2 3 4 5 6) by #'cddr
-  when value
-  collect (list key value))
-=> ((1 2) (3 4) (5 6))
-
-|#
-
 
 #|
 ;; function not necessary -- use undocumented rnd-pick arg :encode nil
@@ -70,18 +42,4 @@ Note: doing the same with loop
             ((h. h) (h))) :encode nil)
 |#
 
-
-(defun apropos-function-documentation (my-string &optional (package *package*))
-  "Lists all functions that contain `my-string' alongside their documentation in a list of pairs
-  (<function-symbol> <doc-string>)
-
-  Examples:
-  ;;; (apropos-function-documentation \"omn\")"
-  (mapcar #'(lambda (x) (list x (documentation x 'function)))
-          (remove-if-not #'fboundp (apropos-list my-string package))))
-
-; (apropos-function-documentation "omn")
-; (apropos-function-documentation "velocity")
-; (apropos-function-documentation "chord")
-; (apropos-function-documentation "-ties")
 
